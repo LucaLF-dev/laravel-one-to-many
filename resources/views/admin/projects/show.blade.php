07786f9a43d23d2701f2b64aab60de49aa8e9f35
LucaLF-dev <139238422+LucaLF-dev@users.noreply.github.com>
2024-01-09 20:02:30 +0100

    Add - model migration seeder and controller Type

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -7,16 +7,17 @@
         <h1>{{$project->title}}</h1>
         <p>{{$project->description}}</p>
     </section>
-    <div class="text-center mb-2">
-        <a class="text-light text-decoration-none btn btn-primary" href="{{route('admin.projects.index')}}">I miei Progetti</a>
 
-    </div>
-    <div class="text-center">
+    <div class=" d-flex justify-content-center m-3 gap-2">
 
         <button type="submit" class="btn btn-danger"  data-bs-toggle="modal" data-bs-target="#staticBackdrop-{{$project->id}}">
             Elimina
         </button>
         <a href="{{route('admin.projects.edit', $project->id)}}" class="btn btn-success">Modifica</a>
+    </div>
+    <div class="text-center mt-6">
+        <a class="text-light text-decoration-none btn btn-primary" href="{{route('admin.projects.index')}}">Ritorna ai miei Progetti</a>
+
     </div>
     <div class="modal fade" id="staticBackdrop-{{$project->id}}" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
         <div class="modal-dialog">
